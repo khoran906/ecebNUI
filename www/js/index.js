feb813d1f06384da44effa445ed0f3df856801d7
Doraknot <anushrisingh24@gmail.com>
2019-05-14 10:36:08 -0400

    New UI
    framework7

--- www/js/index.js
+++ www/js/index.js
@@ -16,28 +16,22 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app;
-app = {
+var app = {
     // Application Constructor
-    initialize: function () {
+    initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
-    onDeviceReady: function () {
-        app.receivedEvent('deviceready');
-        app.pluginInitialize();
-        app.bindNotificationEvents();
 
-
-        // deviceready Event Handler
-        //
-        // Bind any cordova events here. Common events are:
-        // 'pause', 'resume', etc.
-        //onDeviceReady: function() {
-        //this.receivedEvent('deviceready');
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function (id) {
+    receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -46,54 +40,7 @@ app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    },
-    bindNotificationEvents: function () {
-        cordova.plugins.notification.local.on('schedule', function (obj) {
-            console.log('schedule', arguments);
-            // showToast('scheduled: ' + obj.id);
-        });
-        cordova.plugins.notification.local.on('update', function (obj) {
-            console.log('update', arguments);
-            showToast('updated: ' + obj.id);
-        });
-        cordova.plugins.notification.local.on('trigger', function (obj) {
-            console.log('trigger', arguments);
-            showToast('triggered: ' + obj.id);
-        });
-        cordova.plugins.notification.local.on('click', function (obj) {
-            console.log('click', arguments);
-            showToast('clicked: ' + obj.id);
-        });
-        cordova.plugins.notification.local.on('cancel', function (obj) {
-            console.log('cancel', arguments);
-            // showToast('canceled: ' + obj.id);
-        });
-        cordova.plugins.notification.local.on('clear', function (obj) {
-            console.log('clear', arguments);
-            showToast('cleared: ' + obj.id);
-        });
-        cordova.plugins.notification.local.on('cancelall', function () {
-            console.log('cancelall', arguments);
-            // showToast('canceled all');
-        });
-        cordova.plugins.notification.local.on('clearall', function () {
-            console.log('clearall', arguments);
-            // showToast('cleared all');
-        });
-        cordova.plugins.notification.local.on('like', function () {
-            console.log('like', arguments);
-            showToast('liked');
-        });
-        cordova.plugins.notification.local.on('dislike', function () {
-            console.log('dislike', arguments);
-            showToast('disliked');
-        });
-        cordova.plugins.notification.local.on('feedback', function (obj, e) {
-            console.log('feedback', arguments);
-            showToast('Feedback: ' + e.text);
-        });
     }
-
-    };
+};
 
 app.initialize();
